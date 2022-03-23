f53bb9304e0f492be89a5cbeb88385b47b473c7e
Frank Lee <plat4m@outlook.com>
2022-03-22 17:06:18 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (num, num2) => {
+    num = 2000;
+    num2 = 100;
+    return num / num2;
+}
+
+
+const square = (num) => num * num;
+square(5);
+
+
+const add = (num, num2) => num + num2;
+add(6,6);
